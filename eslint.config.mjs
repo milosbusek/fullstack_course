f95dce971433503c90c1d78de626e12609ba4077
milos <milosbusek@gmail.com>
2025-07-13 16:50:52 +0200

    ukol1

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "@eslint/css";
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);npm install -D @stylistic/eslint-plugin
+
+
+
